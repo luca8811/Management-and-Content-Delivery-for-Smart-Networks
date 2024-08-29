bfbddb76cc8153617586a4bb307d9077c08d7ecb
steurendo <steurendoit@gmail.com>
2024-08-29 11:48:27 +0200

    Lab2: changed measurements structure, implemented no-battery drones

--- utils/measurements.py
+++ utils/measurements.py
@@ -1,87 +1,39 @@
-class Statistics:
+import copy
+
+
+class Measurement:
+    """
+    Represents a collection of different measurements (such as arrivals, departures, etc..) taken
+    at a specified time.
+    """
+
     def __init__(self):
-        self.arr = 0
-        self.dep = 0
-        self.los = 0
+        self.time = 0  # changed from 'oldT'
+        self.arrivals = 0  # changed from 'arr'
+        self.departures = 0  # changed from 'dep'
+        self.losses = 0  # changed from 'los'
         self.users = 0
         self.drones = 0
         self.charging_drones = 0
-        self.ut = 0
-        self.oldT = 0
+        self.average_users = 0  # changed from ut
         self.delay = 0
 
 
 class Measurements:
+    """
+    Represents the full collection of all the measurements taken over time. Used to gather
+    information about how the simulation evolved and to plot the results
+    """
+
     def __init__(self):
-        self.users = []
-        self.drones = []
-        self.charging_drones = []
-        self.arrivals = []
-        self.departures = []
-        self.losses = []
-        self.delay = []
+        self.history = [Measurement()]
+        self._last_measurement = self.history[-1]
 
-    def add_record(self, time, data: Statistics):
-        self.users.append((time, data.users))
-        self.drones.append((time, data.drones))
-        self.charging_drones.append((time, data.charging_drones))
-        self.arrivals.append((time, data.arr))
-        self.departures.append((time, data.dep))
-        self.losses.append((time, data.los))
-        self.delay.append((time, data.delay))
+    def get_last_measurement(self):
+        return self._last_measurement
 
-    def dispatch_users(self, starting_time):
-        times = []
-        users_t = []
-        for time, users in self.users:
-            times.append((time + starting_time) / 3600)
-            users_t.append(users)
-        return times, users_t
+    def get_last_time(self):
+        return self._last_measurement.time
 
-    def dispatch_drones(self, starting_time):
-        times = []
-        drones_t = []
-        for time, drones in self.drones:
-            times.append((time + starting_time) / 3600)
-            drones_t.append(drones)
-        return times, drones_t
-
-    def dispatch_charging_drones(self, starting_time):
-        times = []
-        charging_drones_t = []
-        for time, charging_drones in self.charging_drones:
-            times.append((time + starting_time) / 3600)
-            charging_drones_t.append(charging_drones)
-        return times, charging_drones_t
-
-    def dispatch_arrivals(self, starting_time):
-        times = []
-        arrivals_t = []
-        for time, arrivals in self.arrivals:
-            times.append((time + starting_time) / 3600)
-            arrivals_t.append(arrivals)
-        return times, arrivals_t
-
-    def dispatch_departures(self, starting_time):
-        times = []
-        departures_t = []
-        for time, departures in self.departures:
-            times.append((time + starting_time) / 3600)
-            departures_t.append(departures)
-        return times, departures_t
-
-    def dispatch_losses(self, starting_time):
-        times = []
-        losses_t = []
-        for time, losses in self.losses:
-            times.append((time + starting_time) / 3600)
-            losses_t.append(losses)
-        return times, losses_t
-
-    def dispatch_delay(self, starting_time):
-        times = []
-        delay_t = []
-        for time, delay in self.delay:
-            times.append((time + starting_time) / 3600)
-            delay_t.append(delay)
-        return times, delay_t
+    def add_measurement(self, measurement: Measurement):
+        self.history.append(copy.deepcopy(measurement))
