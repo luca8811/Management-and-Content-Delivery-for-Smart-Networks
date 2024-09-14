3faad8759ae34be032e9b6416369729c49e273fe
Marzio Reitano <marzioreitano@gmail.com>
2024-09-14 13:55:22 +0200

    Finito task 3.1 lab 2.

--- lab2/task2.py
+++ lab2/task2.py
@@ -2,38 +2,41 @@ import random
 from queue import PriorityQueue
 import lab2
 import results_visualization
-from lab2 import Event, evt_arrival, evt_departure, evt_recharge, evt_switch_off
+from lab2 import (Event, evt_arrival, evt_departure, evt_recharge, evt_switch_off, clear_folder)
 from utils.queues import MMmB
 
+# Clear the folder where report images will be stored to ensure fresh output.
+clear_folder('./report_images')
+
+# Initialize variables and configurations for the simulation
 lab2.init_variables("TASK2")
 variables = lab2.variables
 MMms = lab2.MMms
 measurements = lab2.measurements
 data = lab2.data
 
-drone_types = variables['drone_types']
-for i, drone_type in enumerate(variables['configurations']['I']):
-    drone = drone_types[drone_type]
-    MMms[i] = MMmB(power_supply=drone['POW'],
-                   service_times=[1 / (variables['BASE_SERVICE_RATE'] * drone['SERVICE_RATE'])
-                                  for m in range(drone['m_ANTENNAS'])],
-                   buffer_size=variables['BASE_BUFFER_SIZE'] * drone['BUFFER_SIZE'])
 
+# Function to run the simulation for a given WORKING_SCHEDULING configuration
+def run_simulation(scheduling_key):
+    # Initialize drone configurations based on the selected WORKING_SCHEDULING value
+    drone_types = variables['drone_types']
+    for i, drone_type in enumerate((variables['configurations']['I'])):
+        drone = drone_types[drone_type]
+        MMms[i] = MMmB(
+            power_supply=drone['POW'],
+            service_times=[1 / (variables['BASE_SERVICE_RATE'] * drone['SERVICE_RATE']) for m in
+                           range(drone['m_ANTENNAS'])],
+            buffer_size=variables['BASE_BUFFER_SIZE'] * drone['BUFFER_SIZE'],
+            maximum_recharge_cycles=variables["RECHARGE_CONSTRAINT"]["I"],
+            working_slots=variables[f"WORKING_SCHEDULING"][scheduling_key]  # Use the specified WORKING_SCHEDULING
+        )
 
-if __name__ == '__main__':
+    # Simulation logic (same as before)
     random.seed(42)
-
-    # the simulation time
     time = 0
-
-    # the list of events in the form: (time, evt_type, drone_id*, additional_parameters*)
-    # * -> if needed
     FES = PriorityQueue()
-
-    # schedule the first arrival at t=0, in order to make the simulation start.
     FES.put((0, Event.ARRIVAL, None, None))
 
-    # simulate until the simulated time reaches a constant
     while time < variables['SIM_TIME']:
         (time, event_type, drone_id, arg) = FES.get()
 
@@ -46,24 +49,30 @@ if __name__ == '__main__':
         elif event_type == Event.RECHARGE:
             evt_recharge(time, drone_id)
 
-    results_visualization.STARTING_TIME = variables['STARTING_TIME']
-    results_visualization.plot_users(measurements)
-    results_visualization.plot_arrivals(measurements)
-    results_visualization.plot_departures(measurements)
-    results_visualization.plot_losses(measurements)
-    results_visualization.plot_dep_los(measurements)
-    results_visualization.plot_drones(measurements)
-    results_visualization.plot_delay(measurements)
-    results_visualization.plot_average_delay_over_time_logarithmic(measurements)
+    print(f"\nResults for WORKING_SCHEDULING '{scheduling_key}':")
 
-    # print output data
-    print("MEASUREMENTS \n\nNo. of users in the queue:", data.users, "\nNo. of arrivals =",
+    # Visualize and print the results for each WORKING_SCHEDULING configuration
+    results_visualization.plot_users(measurements)
+
+    # Print output data for comparison
+    print("\nMEASUREMENTS \n\nNo. of users in the queue:", data.users, "\nNo. of arrivals =",
           data.arrivals, "- No. of departures =", data.departures)
 
-    print("\nArrival rate: ", data.arrivals / time, " - Departure rate: ", data.departures / time)
-    print("Loss rate: ", data.losses / time, " - Packets loss: ", data.losses)
-    print("Departures-losses ratio: ", data.departures / data.losses)
-    print("Departures percentage: {:.1f}%".format(data.departures / data.arrivals * 100))
+    print("\nArrival rate: ", data.arrivals / variables['SIM_TIME'], " - Departure rate: ",
+          data.departures / variables['SIM_TIME'])
+    print("Loss rate: ", data.losses / variables['SIM_TIME'], " - Packets loss: ", data.losses)
+    print("Departures-losses ratio: ", data.departures / data.losses if data.losses > 0 else "N/A")
+    print("Departures percentage: {:.1f}%".format(data.departures / data.arrivals * 100 if data.arrivals > 0 else 0))
 
-    print("\nAverage number of users: ", data.average_users / time)
-    print("Average delay: ", data.delay / data.departures)
+    print("\nAverage number of users: ", data.average_users / variables['SIM_TIME'])
+    print("Average delay: ", data.delay / data.departures if data.departures > 0 else "N/A")
+    print("\n**** **** ****")
+
+    return measurements, data
+
+
+# Run the simulation for each WORKING_SCHEDULING configuration
+for scheduling_key in variables[f"WORKING_SCHEDULING"]:
+
+    # Run the simulation and get the results
+    measurements, data = run_simulation(scheduling_key)
