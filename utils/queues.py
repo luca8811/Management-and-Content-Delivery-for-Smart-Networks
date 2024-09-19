a5b49e8010cd5315e1ed946a8d377a7480daf82f
lucabernardi <lucabernardi881@gmail.com>
2024-09-19 17:08:26 +0200

    modifiche task 1 per buffer =0.

--- utils/queues.py
+++ utils/queues.py
@@ -124,11 +124,12 @@ class MMmB:
         Determine if any servers are available to start serving a packet.
         """
         n_servers_w = self._get_servers_working()
-        # If buffer_size is 0, we check if there's an immediate slot for processing.
-        return self._get_servers_working() < min(len(self._servers), self.queue_size())
-        # if self.buffer_size == 0:
-        #     return n_servers_w < len(self._servers)
-        # return n_servers_w < len(self._servers) and n_servers_w < self.queue_size()
+        if self.buffer_size == 0:
+            # Se non c'è buffer, controlla semplicemente se esiste un server libero
+            return n_servers_w < len(self._servers)
+        else:
+            # Se c'è un buffer, controlla sia la disponibilità del server che lo spazio nel buffer
+            return n_servers_w < len(self._servers) and n_servers_w < self.queue_size()
 
     def _get_server_random(self):
         return random.choice(self._get_available_servers())
